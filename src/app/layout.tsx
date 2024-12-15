f06402b9ea39082d0543829b84c1a553d09ab7c0
Aseel AlAseel <aseelalali81@gmail.com>
2024-12-15 19:37:01 +0200

    add GoogleAnalytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import ThemeRegistry from "@/components/ThemeRegistry/ThemeRegistry";
 import "../styles/globals.css";
 import { IBM_Plex_Sans } from "next/font/google";
+import { GoogleAnalytics } from "@next/third-parties/google";
+import Head from "next/head";
 
 const font = IBM_Plex_Sans({
 	weight: ["100", "200", "300", "400", "500", "600", "700"],
@@ -11,17 +13,18 @@ const layout = ({ children }: { children: React.ReactNode }) => {
 	return (
 		<html lang='ar' suppressHydrationWarning={true} dir='rtl'>
 			<ThemeRegistry direction='rtl'>
-				<head>
+				<Head>
 					<meta
 						name='google-site-verification'
 						content='Z_CVlOktniTFxzGBAkRiR6BEv8fZKQt0ZnbgOt3e_3g'
 					/>
-				</head>
+				</Head>
 				<body
 					className={`${font.className} flex min-h-screen flex-col dark:bg-[#151F34]`}
 				>
 					{children}
 				</body>
+				<GoogleAnalytics gaId={"GTM-TNVX8SCS"} />
 			</ThemeRegistry>
 		</html>
 	);
